05cef2954c1291ccf7f030f8db42617ccba439a9
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2008-11-26 11:28:46 +0000

    Changed: English contact URL.

--- trunk/foot.en.php
+++ trunk/foot.en.php
@@ -1,2 +1,2 @@
-<div class="contact">NTNU, NO-7491 Trondheim. Telephone: +47 73 59 50 00. <a href="http://www.ntnu.no/eng/contactus/">Contact us</a></div>
+<div class="contact">NTNU, NO-7491 Trondheim. Telephone: +47 73 59 50 00. <a href="http://www.ntnu.no/contact">Contact us</a></div>
 <div class="editor">Editorial responsibility:  Director of Information Christian Fossen</div>
