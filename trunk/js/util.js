db4b77b5a68a4a8e8b09dd449df32c09c10916c0
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2010-03-11 12:34:32 +0000

    Removed IE6 warning. Added jQuery. Refactored JS for e-mail deobfuscating and footnote links. Refactored main.php structure to speed things up. Changed trigger for indexer; jQuery AJAX instead of image-bug.

--- trunk/js/util.js
+++ trunk/js/util.js
@@ -1,27 +1,15 @@
-function fix_email(a){
+function deObfuscateEmail(a){
 	var addr = a.href;
 	var addrOld = addr;
-	
 	if(addr.indexOf('mailto:') != -1){
-
-
 		while(addr.indexOf('%5Bat%5D') != -1 || addr.indexOf('[at]') != -1)
 			addr = addr.replace(/( |\%20)(\%5B|\[)at(\%5D|\])( |\%20)/,"@");
-
-
 		while(addr.indexOf('(at)') != -1)
 			addr = addr.replace(/\ *\(at\)\ */,"@");
-
-
 		while(addr.indexOf('%5Bdot%5D') != -1 || addr.indexOf('[dot]') != -1)
 			addr = addr.replace(/( |\%20)(\%5B|\[)dot(\%5D|\])( |\%20)/,".");
-
-
 		while(addr.indexOf('%5Bdash%5D') != -1 || addr.indexOf('[dash]') != -1)
 			addr = addr.replace(/( |\%20)(\%5B|\[)dash(\%5D|\])( |\%20)/,"-");
-
-
-
 		a.href = addr;
 		a.title = addr.substr(7);
 		if(a.innerHTML.indexOf('[at]') != -1 || a.innerHTML.indexOf('%5Bat%5D') != -1)
@@ -29,9 +17,26 @@ function fix_email(a){
 	}
 }
 
-function fixup(){
-	var links = document.getElementsByTagName("a");
-	for (var i=0; i<links.length; i++) {
-		fix_email(links[i]); 
-	}
+function deObfuscateEmails(){
+	var links = jQuery('.columns-float a');
+	for(var i=0; i<links.length; i++)
+		deObfuscateEmail(links[i]); 
+}
+
+
+function prepareSearchField(){
+	jQuery('#query').css('color', '#888');
+	var $searchTextDefault = jQuery('#query').attr('value');
+	jQuery('#query').focusin(function(){
+		jQuery(this).css('color', '#000');
+		if(jQuery(this).attr('value') == $searchTextDefault){
+			jQuery(this).attr('value', '');
+		}
+	});
+	jQuery('#query').focusout(function(){
+		if(jQuery(this).attr('value') == ''){
+			jQuery(this).css('color', '#888');
+			jQuery(this).attr('value', $searchTextDefault);
+		}
+	});
 }
