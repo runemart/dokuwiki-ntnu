368bca3f24212de7e40ba00864d124c54c364df5
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2010-06-22 08:54:50 +0000

    First version of the LifeRay layout complete.

---
+++ tags/2010-06-22-liferaylaunch/head.no.php
@@ -0,0 +1,43 @@
+<div id="bluebar">
+	<div class="marginwrapper">
+		<h1><a href="http://www.ntnu.no" title="Gå til NTNUs startside" accesskey="1">Norges teknisk-naturvitenskapelige universitet</a></h1>
+		<ul id="ntnulenker" title="Felleslenker">
+			<?php echo tpl_languageSelector('no'); ?>
+			<?php echo tpl_languageSelector('en'); ?>
+			<li><a href="http://www.ntnu.no/ub" title="Bibliotek"><span>Bibliotek</a></li>
+			<li><a href="https://innsida.ntnu.no" title="Logg på intranettet">Intranett</a></li>
+		</ul>
+		<div class="clearer"></div>
+	</div>
+</div>
+
+<div id="searchform" class="focus">
+	<form action="http://www.ntnu.no/sok/search.fast" method="get" title="Søk i NTNUs nettsider">
+		<fieldset>
+			<legend class="offscreen">Søk i NTNUs nettsider</legend>
+			<label for="query" accesskey="4"><span class="offscreen">Søk etter:</span></label>
+			<input id="query" type="text" name="s.sm.query" value="Søk etter..." title="Søk" class="query" /><input type="submit" id="sok" value="Søk" class="submit" />
+		</fieldset>
+	</form>
+</div>
+
+<?php //echo tpl_languageSelector('no'); ?>
+
+<div class="marginwrapper">
+	<div id="ntnumenuwrap">
+		<div class="offscreen">
+			<h2 id="globalnav">NTNU-navigasjon</h2>
+		</div>
+		<ul id="ntnumenu" title="NTNU-navigasjon" class="selectedtab_<?php echo tpl_getConf('selectedtab'); ?>">
+			<li id="tab_start"><a href="http://www.ntnu.no" title="Startside for NTNU">Startside <span class="offscreen">for NTNU</span></a></li>
+			<li id="tab_studier"><a href="http://www.ntnu.no/studier" title="Studier ved NTNU">Studier <span class="offscreen">ved NTNU</span></a></li>
+			<li id="tab_student"><a href="http://www.ntnu.no/studitrh" title="Student i Trondheim">Student i Trondheim</a></li>
+			<li id="tab_forskning"><a href="http://www.ntnu.no/forskning" title="Forskning ved NTNU">Forskning <span class="offscreen">ved NTNU</span></a></li>
+			<li id="tab_evu"><a href="http://videre.ntnu.no/pages" title="Etter- og videreutdanning">Etter- og videreutdanning</a></li>
+			<li id="tab_non"><a href="http://www.ntnu.no/naringsliv" title="Næringsliv og nyskapning">N&aelig;ringsliv og nyskapning</a></li>
+			<li id="tab_om" class="selected"><a href="http://www.ntnu.no/omntnu" title="Om NTNU">Om NTNU</a></li>
+			<li id="tab_aktuelt"><a style="border-right: none" href="http://www.ntnu.no/aktuelt" title="Aktuelt">Aktuelt</a></li>
+		</ul>
+		<div class="clearer"></div>
+	</div>
+</div>
