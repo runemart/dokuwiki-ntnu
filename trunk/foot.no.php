96792aef55cd23f0fe17446ba18f8a15b973a382
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2010-06-29 07:36:58 +0000

    Corrected footer links. Added icon to "Admissions".

--- trunk/foot.no.php
+++ trunk/foot.no.php
@@ -1,5 +1,5 @@
 <ul>
-	<li class="kontakt"><a href="http://ntnu.no/om">Kontakt oss</a></li>
+	<li class="kontakt"><a href="http://ntnu.no/kontakt">Kontakt oss</a></li>
 	<li class="personsok"><a href="http://ntnu.no/ansatte">Søk etter ansatte</a></li>
 	<li class="personsok"><a href="http://ntnu.no/studentsok">Søk etter studenter</a></li>
 	<li class="sentralbord">Sentralbord: 73 59 50 00</li>
@@ -7,7 +7,7 @@
 </ul>
 
 <ul>
-	<li class="kart"><a href="http://ntnu.no/kart">Kart over NTNU</a></li>
+	<li class="kart"><a href="http://ntnu.no/ntnukart">Kart over NTNU</a></li>
 	<li class="fakulteter"><a href="http://ntnu.no/fakulteter">Fakulteter og institutter</a></li>
 	<li class="adm"><a href="http://ntnu.no/adm">Administrative enheter</a></li>
 	<li class="ledelsen"><a href="http://ntnu.no/rektor">Ledelsen</a></li>
@@ -15,7 +15,7 @@
 </ul>
 
 <ul>
-	<li class="nettstedkart"><a href="http://ntnu.no/nettstedskart">Nettstedkart</a></li>
+	<li class="nettstedkart"><a href="http://ntnu.no/nettstedkart">Nettstedkart</a></li>
 	<li class="studier"><a href="http://ntnu.no/studier/alle">Studier A til Å</a></li>
 	<li class="emner"><a href="http://ntnu.no/studier/emner">Emner A til Å</a></li>
 	<li class="forskning"><a href="http://ntnu.no/forskning/a_aa">Forskning A til Å</a></li>
@@ -25,5 +25,5 @@
 <ul>
 	<li class="blog"><a href="http://ntnu.no/aktuelt/blogger">Blogger</a></li>
 	<li class="twitter"><a href="http://twitter.com/NTNU">Twitter</a></li>
-	<li class="rss"><a href="http://ntnu.no/aktuelt/blogger">RSS-feed</a></li>
+	<li class="rss"><a href="http://ntnu.no/aktuelt/rss">RSS-feed</a></li>
 </ul>
