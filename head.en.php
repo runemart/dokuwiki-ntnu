d6732f6b571d1f5159d360a540a5ea88f5428391
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2010-11-17 10:01:49 +0000

    Updated page header, search field and tab menu to match official version.

--- head.en.php
+++ head.en.php
@@ -1,12 +1,12 @@
 <div id="bluebar">
 	<div class="marginwrapper">
-		<h1><a href="http://www.ntnu.edu" title="Jump to NTNU's start page" accesskey="1">Norwegian University of Science and Technology</a></h1>
+		<div id="mainpagelink"><a href="http://www.ntnu.edu" title="Jump to NTNU's start page" accesskey="1">Norwegian University of Science and Technology</a></div>
 		<ul id="ntnulenker" title="Common links">
 			<?php echo tpl_languageSelector('no'); ?>
 			<?php echo tpl_languageSelector('en'); ?>
 			<li><a href="http://ntnu.edu/ub" title="NTNU Library">Library</a></li>
 			<li><a href="http://alumni.ntnu.no" title="Alumni NTNU">Alumni</a></li>
-			<li><a href="https://innsida.ntnu.no" title="Log on to the intranet">Intranet</a></li>
+			<li class="last"><a href="https://innsida.ntnu.no" title="Log on to the intranet">Intranet</a></li>
 		</ul>
 		<div class="clearer"></div>
 	</div>
@@ -15,9 +15,10 @@
 <div id="searchform" class="focus">
 	<form action="http://ntnu.no/sok/search.fast" method="get" title="Search NTNU web pages">
 		<fieldset>
-			<legend class="offscreen">Search NTNU web pages</legend>
+			<legend class="offscreen">Search web pages at NTNU</legend>
 			<label for="query" accesskey="4"><span class="offscreen">Search for:</span></label>
-			<input id="query" type="text" name="s.sm.query" value="Search for..." title="Search" class="query" /><input type="submit" id="sok" value="Search" />
+			<input id="query" type="text" name="s.sm.query" value="Search for content/employees" title="Search" class="query" />
+			<input type="submit" id="sok" value="Search" />
 			<input type="hidden" name="lang" value="en" />
 		</fieldset>
 	</form>
@@ -31,10 +32,10 @@
 			<h2 id="globalnav">NTNU navigation</h2>
 		</div>
 		<ul id="ntnumenu" title="NTNU navigation" class="selectedtab_<?php echo tpl_getConf('selectedtab'); ?>">
-			<li id="tab_start"><a href="http://www.ntnu.edu" title="Home page for NTNU">Home page <span class="offscreen">for NTNU</span></a></li>
-			<li id="tab_studier"><a href="http://www.ntnu.edu/studies" title="Studies at NTNU">Studies <span class="offscreen">at NTNU</span></a></li>
+			<li id="tab_start"><a href="http://www.ntnu.edu" title="Home page for NTNU">Home</a></li>
+			<li id="tab_studier"><a href="http://www.ntnu.edu/studies" title="Studies at NTNU">Studies</a></li>
 			<li id="tab_student"><a href="http://www.ntnu.edu/livingintrh" title="Living in Trondheim">Living in Trondheim</a></li>
-			<li id="tab_forskning"><a href="http://www.ntnu.edu/research" title="Research at NTNU">Research <span class="offscreen">at NTNU</span></a></li>
+			<li id="tab_forskning"><a href="http://www.ntnu.edu/research" title="Research at NTNU">Research</a></li>
 			<li id="tab_non"><a href="http://www.ntnu.edu/business" title="Business and innovation">Business and innovation</a></li>
 			<li id="tab_om" class="selected"><a href="http://www.ntnu.edu/aboutntnu" title="About NTNU">About NTNU</a></li>
 			<li id="tab_aktuelt"><a style="border-right: none" href="http://www.ntnu.edu/contact" title="Contact us">Contact us</a></li>
--- head.no.php
+++ head.no.php
@@ -1,12 +1,12 @@
 <div id="bluebar">
 	<div class="marginwrapper">
-		<h1><a href="http://ntnu.no" title="Gå til NTNUs startside" accesskey="1">Norges teknisk-naturvitenskapelige universitet</a></h1>
+		<div id="mainpagelink"><a href="http://ntnu.no" title="Gå til NTNUs startside" accesskey="1">Norges teknisk-naturvitenskapelige universitet</a></div>
 		<ul id="ntnulenker" title="Felleslenker">
 			<?php echo tpl_languageSelector('no'); ?>
 			<?php echo tpl_languageSelector('en'); ?>
 			<li><a href="http://ntnu.no/ub" title="Bibliotek">Bibliotek</a></li>
 			<li><a href="http://alumni.ntnu.no" title="Alumni NTNU">Alumni</a></li>
-			<li><a href="https://innsida.ntnu.no" title="Logg på intranettet">Intranett</a></li>
+			<li class="last"><a href="https://innsida.ntnu.no" title="Logg på intranettet">Intranett</a></li>
 		</ul>
 		<div class="clearer"></div>
 	</div>
@@ -17,7 +17,8 @@
 		<fieldset>
 			<legend class="offscreen">Søk i NTNUs nettsider</legend>
 			<label for="query" accesskey="4"><span class="offscreen">Søk etter:</span></label>
-			<input id="query" type="text" name="s.sm.query" value="Søk etter..." title="Søk" class="query" /><input type="submit" id="sok" value="Søk" class="submit" />
+			<input id="query" type="text" name="s.sm.query" value="Søk etter innhold/ansatte" title="Søk" class="query" />
+			<input type="submit" id="sok" value="Søk" class="submit" />
 		</fieldset>
 	</form>
 </div>
@@ -26,16 +27,13 @@
 
 <div class="marginwrapper">
 	<div id="ntnumenuwrap">
-		<div class="offscreen">
-			<h2 id="globalnav">NTNU-navigasjon</h2>
-		</div>
 		<ul id="ntnumenu" title="NTNU-navigasjon" class="selectedtab_<?php echo tpl_getConf('selectedtab'); ?>">
-			<li id="tab_start"><a href="http://ntnu.no" title="Startside for NTNU">Startside <span class="offscreen">for NTNU</span></a></li>
-			<li id="tab_studier"><a href="http://ntnu.no/studier" title="Studier ved NTNU">Studier <span class="offscreen">ved NTNU</span></a></li>
+			<li id="tab_start"><a href="http://ntnu.no" title="Startside for NTNU">Startside</a></li>
+			<li id="tab_studier"><a href="http://ntnu.no/studier" title="Studier ved NTNU">Studier</a></li>
 			<li id="tab_student"><a href="http://ntnu.no/student" title="Student i Trondheim">Student i Trondheim</a></li>
-			<li id="tab_forskning"><a href="http://ntnu.no/forskning" title="Forskning ved NTNU">Forskning <span class="offscreen">ved NTNU</span></a></li>
+			<li id="tab_forskning"><a href="http://ntnu.no/forskning" title="Forskning ved NTNU">Forskning</a></li>
 			<li id="tab_evu"><a href="http://videre.ntnu.no/pages" title="Etter- og videreutdanning">Etter- og videreutdanning</a></li>
-			<li id="tab_non"><a href="http://ntnu.no/naringsliv" title="Næringsliv og nyskapning">N&aelig;ringsliv og nyskapning</a></li>
+			<li id="tab_non"><a href="http://ntnu.no/naringsliv" title="Næringsliv og nyskaping">N&aelig;ringsliv og nyskaping</a></li>
 			<li id="tab_om" class="selected"><a href="http://ntnu.no/om" title="Om NTNU">Om NTNU</a></li>
 			<li id="tab_aktuelt"><a style="border-right: none" href="http://ntnu.no/aktuelt" title="Aktuelt">Aktuelt</a></li>
 		</ul>
