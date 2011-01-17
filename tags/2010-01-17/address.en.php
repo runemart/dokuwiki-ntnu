9e65a7538284e39a7338c756c7faf76939bea8ad
webmaster@ime.ntnu.no <webmaster@ime.ntnu.no@d38cb84e-a019-11dd-b4aa-53559073bf4c>
2011-01-17 09:10:21 +0000

    Deleted ill-named tag.

--- tags/2010-01-17/address.en.php
+++
@@ -1,2 +0,0 @@
-Visiting address: <?= tpl_getConf('visitingaddress_en')?><br />
-Phone: <?=tpl_getConf('phone_en')?> &sdot; Fax: <?=tpl_getConf('fax_en')?> &sdot; E-mail: <a class="mail" href="mailto:<?=tpl_getConf('email')?>"><?=tpl_getConf('email_en')?></a>
